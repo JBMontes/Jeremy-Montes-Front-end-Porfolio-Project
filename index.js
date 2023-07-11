cae682310500fa0585f0546eec44c7f85e0bd39c
Jeremy Montes <jeremymontes@pursuit.org>
2023-07-10 20:00:00 -0400

    made edits to form

--- index.js
+++ index.js
@@ -1,36 +1,108 @@
-function displayCard({fruits}){
-    for(let specifics of fruits){
-     let card = document.createElement("div")
-     card.classList.add("fruit")
-     let specs = document.createElement("ul")
-     let name = document.createElement("li")
-     name.innerText = specifics.name
-     let family = document.createElement("li")
-     family.innerText = specifics.family
-     let order = document.createElement("li")
-     order.innerText = specifics.order
-     let genus = document.createElement("li")
-     genus.innerText = specifics.genus
-     let calories = document.createElement("li")
-     calories.innerText = specifics.calories
-     let fat= document.createElement("li")
-     fat.innerText = specifics.fat
-     let sugar = document.createElement("li")
-     sugar.innerText = specifics.sugar
-     let protein = document.createElement("li")
-     protein.innerText = specifics.protein
-     let carbs = document.createElement("li")
-     carbs.innerText = specifics.carbohydrates
-        card.append(specs,name,family,order,genus,calories,fat,sugar,protein, carbs)
-     let display = document.querySelector(".display")
-     display.append(card)
-    }
+
+function displayCard(results){
+    let div =  document.createElement("div")
+    div.classList.add("number")
+    let number = document.createElement("p")
+    number.classList.add("num")
+    number.innerText = results.number
+    let type = document.createElement("p")
+    type.classList.add("type")
+    type.innerText = results.type.toUpperCase()
+    let text = document.createElement("p")
+    text.classList.add("text")
+    text.innerText = results.text
+    div.append(type, number,  text)
+   let display = document.querySelector(".display")
+   display.append(div)
 
 }
 
-fetch("https://www.fruityvice.com/api/fruit/all")
-  .then((response) => response.json())
-  .then((JSONresponse) => console.log(JSONresponse))
-  .catch((err) => {
+
+fetch("http://numbersapi.com/random/year?json")
+.then((response)=>response.json())
+.then((JSONresponse)=> {return displayCard(JSONresponse)})
+
+.catch((err)=>{ 
     console.log(err);
-  });
+ });
+
+ function displayCard2(results){
+    let div =  document.createElement("div")
+    div.classList.add("number")
+    let number = document.createElement("p")
+    number.classList.add("num")
+    number.innerText = results.number
+    let type = document.createElement("p")
+    type.classList.add("type")
+    type.innerText = results.type.toUpperCase()
+    let text = document.createElement("p")
+    text.classList.add("text")
+    text.innerText = results.text
+    div.append(type, number,  text)
+   let display = document.querySelector(".display")
+   display.append(div)
+
+}
+
+fetch("http://numbersapi.com/random/year?json")
+.then((response)=>response.json())
+.then((JSONresponse)=> {return displayCard2(JSONresponse)})
+
+.catch((err)=>{ 
+    console.log(err);
+ });
+
+ function displayCard3(results){
+    let div =  document.createElement("div")
+    div.classList.add("number")
+    let number = document.createElement("p")
+    number.classList.add("num")
+    number.innerText = results.number
+    let type = document.createElement("p")
+    type.classList.add("type")
+    type.innerText = results.type.toUpperCase()
+    let text = document.createElement("p")
+    text.classList.add("text")
+    text.innerText = results.text
+    div.append(type, number, text)
+   let display = document.querySelector(".display")
+   display.append(div)
+
+}
+
+fetch("http://numbersapi.com/random/year?json")
+.then((response)=>response.json())
+.then((JSONresponse)=> {return displayCard3(JSONresponse)})
+
+.catch((err)=>{ 
+    console.log(err);
+ });
+let form = document.querySelector("form")
+ form.addEventListener("submit", (e) => {
+    console.log(e.target);
+    e.preventDefault();
+    
+    let div =  document.createElement("div")
+    div.classList.add("number")
+    let year = document.createElement("p")
+    year.classList.add("year")
+    year.innerText = "YEAR"
+    let number = document.createElement("p")
+    number.classList.add("num")
+    number.innerText = e.target["year"].value
+    let fact = document.createElement("p")
+    fact.classList.add("text")
+    fact.innerText = e.target["fact"].value
+    let button = document.createElement("button")
+    button.classList.add("button") 
+    button.innerText = "✖︎"
+    button.addEventListener("click", (e)=>{
+        e.preventDefault();
+        e.target.parentElement.remove()
+    })
+    div.append(year, number, fact, button)
+    document.querySelector(".display").append(div)
+
+    e.target.reset();
+
+ })
